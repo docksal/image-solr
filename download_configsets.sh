f3fb906f9c90cfa2045a4687c23cf304c5ab7854
Sergey Zabolotny <sergey.zabolotny@ffwagency.com>
2018-11-26 12:23:51 +0200

    Delete unncessary code

--- download_configsets.sh
+++ download_configsets.sh
@@ -16,7 +16,6 @@ do
   eval $module
   for tag in $(echo $tags | sed 's#,# #g')
   do
-    #echo $module_name $tag
     git clone -b $tag git@github.com:drupalprojects/${module_name}.git tmp >/dev/null 2>&1
     dirs=$(find tmp/solr-conf/ -maxdepth 1 -type d -path "*.x")
     for dir in ${dirs}
@@ -27,31 +26,6 @@ do
       cp "${dir}"/* "${conf_dir}"
     done
     rm -rf tmp
-  done
-done
-
-
-exit
-
-for module_name in "search_api_solr" "apachesolr"
-do
-  for drupal in "8.x" "7.x"
-  do
-    versions=$(find ../configsets/${module_name}/ -maxdepth 1 -path *${drupal}*|rev|cut -d'/' -f1|rev)
-    for version in ${versions}
-    do
-      dir="../configsets/${module_name}/${version}/conf/${SOLR_VER:0:1}.x"
-      if [[ -d "${dir}" ]]
-      then
-        echo "${module_name} ${version}: adding config set for ${SOLR_VER:0:1}.x"
-
-        conf_dir="configsets/${version}_${module_name}/conf"
-        mkdir -p "${conf_dir}"
-        cp "${dir}"/* "${conf_dir}"
-      else
-        echo "${module_name} ${version}: does not support Solr ${SOLR_VER:0:1}.x"
-      fi
-    done
   done
 done
 
